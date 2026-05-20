e4eb868405fc069d38a5070ec514a39a3912e0b6
Cabdi Waaxid Siciid <158821136+cabdiwaaxid-so@users.noreply.github.com>
2026-05-20 09:03:36 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,1 +1,16 @@
+const novax = require('novaxjs2');
+const app = new novax();
+app.cors({
+	origins: ['*']
+});
+app.get('/', (req, res) => {
+  res.status(204).json({
+    message: "No Content"
+  });
+});
+app.at(3000, () => console.log('App is running on port 3000'));
 
+// for vercel serverless
+module.exports = (req, res) => {
+  app.server.emit('request', req, res);
+};
